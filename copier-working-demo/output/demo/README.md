b66dd72d14c7001bdc1422de15c9b2ddc9a01715
shreyaskn72 <shreyaskn72@gmail.com>
2026-01-05 17:35:55 +0530

    copier

--- copier-working-demo/output/demo/README.md
+++
@@ -1,4 +0,0 @@
-# {{ project_name }}
-
-Created by {{ author }}  
-Generated using Copier template.
--- copier-working-demo/output/demo/app.py
+++
@@ -1,5 +0,0 @@
-def main():
-    print("Hello from {{ project_name }}")
-
-if __name__ == "__main__":
-    main()
